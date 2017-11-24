bd9c47e8df27342e8ab434ff05efae8c7684a8eb
root <root@damowill2.mylabserver.com>
2017-11-24 15:20:57 +0000

    adding files

--- cookbooks/apache/recipes/website.rb
+++ cookbooks/apache/recipes/website.rb
@@ -2,3 +2,10 @@ file 'default www' do
 	path '/var/www/html/index.html'
 	content 'test server-workstation'
 end
+
+webnodes = search('node', 'recipes:apache\:\:default')
+#webnodes = search('node', 'role:web')
+
+webnodes.each do |nodes|
+	puts nodes
+end
